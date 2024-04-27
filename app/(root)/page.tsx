5e8cf2f8125ef7fd6341ac2b0cbedef26b5380fb
911samuel <abayizeraeaz@gmail.com>
2024-04-27 22:29:20 +0200

    Ft-components
    creating all neccesary component

---
+++ app/(root)/page.tsx
@@ -0,0 +1,75 @@
+import CategoryFilter from '@/components/shared/CategoryFilter';
+import Collection from '@/components/shared/Collection'
+import Search from '@/components/shared/Search';
+import { Button } from '@/components/ui/button'
+import { getAllEvents } from '@/lib/actions/event.actions';
+import { SearchParamProps } from '@/types';
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
+
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6
+  })
+
+  return (
+    <>
+      {/* Hero section */}
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-10 md:py-20">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">
+              Discover, Connect, Celebrate: Events Made Easy!
+            </h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Explore and Learn from Thousands of Events Hosted by Industry
+              Experts.
+            </p>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">Explore Now</Link>
+            </Button>
+          </div>
+
+          <Image
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          />
+        </div>
+      </section>
+
+      <section
+        id="events"
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12"
+      >
+        <h2 className="h2-bold">
+          Trusted by <br /> Thousands of Event Organizers
+        </h2>
+
+        <div className="flex flex-col md:flex-row gap-5 w-full">
+          <Search />
+          <CategoryFilter />
+        </div>
+
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Check back later for more exciting events!"
+          collectionType="All_Events"
+          limit={6}
+          page={page}
+          totalPages={events?.totalPages}
+        />
+      </section>
+    </>
+  );
+}
